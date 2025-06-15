04f9504e4b52f65e147c27d644bb4db61409a67c
Pratibha Agrawal <125815236+pratibha12-34@users.noreply.github.com>
2025-06-15 17:49:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,91 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+class MinHeap {
+    constructor() {
+        this.heap = [];
+    }
+
+    insert(val) {
+        this.heap.push(val);
+        this.bubbleUp();
+    }
+
+    bubbleUp() {
+        let index = this.heap.length - 1;
+        while (index > 0) {
+            let element = this.heap[index];
+            let parentIndex = Math.floor((index - 1) / 2);
+            let parent = this.heap[parentIndex];
+
+            if (parent <= element) break;
+            this.heap[index] = parent;
+            this.heap[parentIndex] = element;
+            index = parentIndex;
+        }
+    }
+
+    extractMin() {
+        if (this.heap.length === 1) return this.heap.pop();
+        const min = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this.sinkDown(0);
+        return min;
+    }
+
+    sinkDown(index) {
+        const length = this.heap.length;
+        const element = this.heap[index];
+
+        while (true) {
+            let leftChildIdx = 2 * index + 1;
+            let rightChildIdx = 2 * index + 2;
+            let leftChild, rightChild;
+            let swap = null;
+
+            if (leftChildIdx < length) {
+                leftChild = this.heap[leftChildIdx];
+                if (leftChild < element) {
+                    swap = leftChildIdx;
+                }
+            }
+
+            if (rightChildIdx < length) {
+                rightChild = this.heap[rightChildIdx];
+                if (
+                    (swap === null && rightChild < element) ||
+                    (swap !== null && rightChild < leftChild)
+                ) {
+                    swap = rightChildIdx;
+                }
+            }
+
+            if (swap === null) break;
+            this.heap[index] = this.heap[swap];
+            this.heap[swap] = element;
+            index = swap;
+        }
+    }
+
+    size() {
+        return this.heap.length;
+    }
 }
 
-module.exports=mincost;
+function mincost(arr) { 
+    let heap = new MinHeap();
+    for (let num of arr) {
+        heap.insert(num);
+    }
+
+    let cost = 0;
+
+    while (heap.size() > 1) {
+        let first = heap.extractMin();
+        let second = heap.extractMin();
+        let currentCost = first + second;
+        cost += currentCost;
+        heap.insert(currentCost);
+    }
+
+    return cost;
+}
+
+module.exports = mincost;
